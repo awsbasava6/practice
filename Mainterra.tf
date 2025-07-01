14f1e7554b5914f70abf4817b6aa2033f4af9634
awsbasava6 <168918546+awsbasava6@users.noreply.github.com>
2025-07-01 20:18:01 +0530

    added terraform files

---
+++ Mainterra.tf
@@ -0,0 +1,16 @@
+ource "aws_security_group" "Swiggy-SG" {
+  name        = "Swiggy-SG"
+  description = "Open 22,443,80,8080,9000"
+
+  # Define a single ingress rule to allow traffic on all specified ports
+  ingress = [
+    for port in [22, 80, 443, 8080, 9000, 3000] : {
+      description      = "TLS from VPC"
+      from_port        = port
+      to_port          = port
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
